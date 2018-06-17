16e16204415171a40f3ecf2b2c8880ce5f40a2b5
Andrew Lien <andrew.lien1130@gmail.com>
2018-06-17 16:17:29 -0700

    Added new functionality for update & authentication

--- mail_db.py
+++ mail_db.py
@@ -9,7 +9,7 @@ import imaplib
 from email.parser import HeaderParser
 import json
 from copy import deepcopy
-
+import os
 class GmailHandler():
     @staticmethod
     def get_smtp_info():
@@ -42,9 +42,30 @@ class Errors():
         return {"msg":"Key exists in DB : {}".format(specific_message)}
 
     @staticmethod
+    def key_does_not_exists(specific_message=None):
+        return {"msg":"Key does exists in DB, maybe you meant to perform an insert? : {}".format(specific_message)}
+
+    @staticmethod
     def invalid_key(specific_message=None):
         return {'valid_key': "Key not valid", 'msg': "Key does not exist in the database", "specific_message": "{} does not exist".format(specific_message)}
 
+    @staticmethod
+    def invalid_creds(specific_message=None):
+        return {
+            "invalid":True,
+            "msg":"Error retrieving email credentials. You have a few options to instantiate your session, ordered by precedence: \n  \
+            1. You may also provide explicit credentials in the form of MailDB(username_string,password_string) \n  \
+            2. You may instantiate MailDB() without any arguments if there exists a ~/.maildb/creds.json that contains the credentials of your email account. \n \
+            \t2a. The credentials file should be in a keyvalue pair format such as: {'user':'yourusername','password':'yourpassword'}\n \
+            3. You may also provide the following environment variables: MAILDB_ENV_USER, MAILDB_ENV_PASSWORD \n \
+            Note: Please make sure you use your email user's full email address such as: maildb@gmail.com or maildb@outlook.com\n"
+        }
+    
+    @staticmethod
+    def invalid_json(specific_message=None):
+        return {
+            "msg":"Credentials file does not pass JSON validation. The credentials file should have the following format: {'user':'yourusername','password':'yourpassword'}"
+        }
 class Helpers():
     @staticmethod
     def retrieve_domain(email_string):
@@ -75,17 +96,71 @@ class Helpers():
         """
 
         return {"valid_key":"Key not valid","msg":""}
+
+    @staticmethod
+    def check_credential_path(credential_path=None):
+        path = None
+        default_credential_path = os.path.join(os.path.expanduser('~'), '.maildb','creds.json')  
+        if credential_path is None:
+            path = default_credential_path
+        else:
+            path = credential_path
+
+        if os.path.isfile(path):
+            creds_file = open(path,'r').read()
+        else:
+            return None
+
+        try:
+            creds_json = json.loads(creds_file)
+        except:
+            Errors.invalid_json()
+            sys.exit()
+        return creds_json
+class Auth(): 
+    def __init__(self):
+        pass  
+
+    def retrieve_auth(self,username=None,password=None,credential_path=None):
+        # If either user or pass is not available, attempt to check using preconfigured JSON variables
+        if username is None or password is None:
+            path_results = Helpers.check_credential_path(credential_path)
+            if path_results is None: 
+                return Errors.invalid_creds()
+            else:
+                user_name = path_results.get('user')
+                pass_word = path_results.get('password')
+        else:
+            user_name = username
+            pass_word = password
+        domain = Helpers.retrieve_domain(user_name)
+
+        auth_obj = {
+            "user_name": user_name,
+            "pass_word": pass_word,
+            "domain":domain
+        }
+        return auth_obj
+
 class MailDB():
-    def __init__(self,username,password):
-        self.user_name = username
-        self.pass_word = password
-        self.domain = Helpers.retrieve_domain(self.user_name)
+    def __init__(self,username=None,password=None,credential_path=None):
         self.logger = logging.getLogger('MailDB')
+
+        auth_obj = Auth.retrieve_auth(username,password,credential_path)
+
+        if auth_obj.get('invalid'):
+            if auth_obj['invalid'] == True:
+                self.logger.error(auth_obj.get('msg'))
+
+        try:
+            self.user_name = auth_obj.get('user_name')
+            self.pass_word = auth_obj.get('pass_word')
+            self.domain = auth_obj.get('domain')
         
-        if 'gmail' in self.domain:
-            self.smtp_ssl_host, self.smtp_ssl_port, self.imap_ssl_host, self.imap_ssl_port = GmailHandler.get_smtp_info()
-
-
+            if 'gmail' in self.domain:
+                self.smtp_ssl_host, self.smtp_ssl_port, self.imap_ssl_host, self.imap_ssl_port = GmailHandler.get_smtp_info()
+        except Exception as e:
+            self.logger.error(e)
 
     def get(self,key=None):
         if not key:
@@ -93,7 +168,12 @@ class MailDB():
 
 
         server = imaplib.IMAP4_SSL(self.imap_ssl_host,self.imap_ssl_port)
-        server.login(self.user_name,self.pass_word)
+      
+        try:
+            server.login(self.user_name,self.pass_word)
+        except:
+            print("ERRORFATAL: Error logging in, perhaps credentials issue? {}".format(e))
+            sys.exit()
         server.select('"[Gmail]/All Mail"')
 
         search_key = 'Subject'
@@ -178,5 +258,44 @@ class MailDB():
         server.sendmail(sender,targets,msg.as_string())
         server.quit()
         return True
+
+    def update(self,key=None,value=None,forced_insert=False):
+        if self.domain == "INVALID":
+            return 'Email State: {}'.format(Errors.bad_email_domain())
+
+        if not key:
+            return Errors.missing_value(specific_message="Requires key")
+        if not value:
+            return Errors.missing_value(specific_message="Requires value")
+
+
+        try:
+            body = json.dumps(value)
+        except Exception as e:
+            return Errors.missing_value(specific_message="Value must be in valid JSON format")
+
+        # Before we do anything, we need to make sure the key doesn't already exist
+        if not self.validate_key(key=key) and forced_insert==False:
+            return Errors.key_does_not_exists(specific_message='{}'.format(key))
+
+
+        # Declare variables to prep for email send
+        subject = key
+        sender = self.user_name
+        targets = self.user_name
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['To'] = targets
+
+        server = smtplib.SMTP_SSL(self.smtp_ssl_host,self.smtp_ssl_port)
+        try:
+            server.login(self.user_name,self.pass_word)
+        except smtplib.SMTPAuthenticationError as e:
+            self.logger.error('Google blocks sign-in attempts from apps that do not use modern security standards. To turn on/off this safety feature, go to https://www.google.com/settings/security/lesssecureapps and select "Turn On"')
+            server.quit()
+            sys.exit(1)
+        server.sendmail(sender,targets,msg.as_string())
+        server.quit()
+        return True 
 
 
--- mail_db_function_test.py
+++ mail_db_function_test.py
@@ -4,16 +4,22 @@ import json
 data = {}
 
 
+
+
+# An example of passing no explicit auth
+mdb = MailDB()
+
+# An example of passing in explicit auth
 with open('C:\\path\\to\\creds\\of_email.json') as f:
-    data = json.load(f)
+   data = json.load(f)
 
 
 mdb = MailDB(data.get('user'),data.get("password"))
 
-
 # Can insert any JSON to the mail db
-insert_key ="key_one"
+insert_key ="key_four"
 insert_value = {
+
                     "one":"two",
                     "three":"four",
                     "four": True,
@@ -23,8 +29,45 @@ insert_value = {
                 }
 
 # An example of an insertion statement
-mdb.insert(key=insert_key,value=insert_value)
+print(mdb.insert(key=insert_key,value=insert_value))
 
 # An example of a GET statement
 raw_data = mdb.get(key=insert_key)
+print (raw_data)
 
+# An example of an UPDATE statement
+update_value = {
+    "new":"value",
+    "test":123
+}
+print (mdb.update(key=insert_key,value=update_value))
+
+# Requerying the database using the same key, but observe with new values
+raw_data = mdb.get(key=insert_key)
+print (raw_data)
+
+
+# An example of an UPDATE statement on a key that does not exist
+new_insertkey = "key_five"
+update_value = {
+    "new":"newer_value",
+    "test":1234
+}
+print (mdb.update(key=new_insertkey,value=update_value))
+
+# Requerying the database using the same key, but observe with new values
+raw_data = mdb.get(key=insert_key)
+print (raw_data)
+
+
+# An example of an UPDATE statement on a key that does not exist, but force the insertion anyway
+new_insertkey = "key_five"
+update_value = {
+    "new":"even_newer_value",
+    "test":12345
+}
+print (mdb.update(key=new_insertkey,value=update_value,forced_insert=True))
+
+# Requerying the database using the same key, but observe with new values
+raw_data = mdb.get(key=new_insertkey)
+print (raw_data)
